5bc20fed507cc4f69a439a3618f972c07ce63e6f
RealKC <mitrut.e.super@gmail.com>
2018-10-22 00:13:16 +0300

    Add a page about our merchandise poll (#34)
    * Add info about merch poll
    
    * fixed url formatting
    
    * I mixed up the brackets
    
    * Merch info

---
+++ public/markdown/merch.md
@@ -0,0 +1,2 @@
+# Merchandise
+We've been thinking about the possibility of selling FredBoat-branded merchandise, and would like to hear our users' feedback on what kind of merchandise they'd want to buy. If you wish to provide feedback, [please answer the poll](https://docs.google.com/forms/d/e/1FAIpQLSckDJrW2uibulMsa0s-x3twToQtaGHT9P35tK9Zzh_5mXQ1UA/viewform?usp=sf_link).
--- src/docs/Toc.js
+++ src/docs/Toc.js
@@ -39,6 +39,7 @@ class Toc extends Component {
                     <TocHeader activePage={this.props.page} page="index" name="Quickstart"/>
                     <TocHeader activePage={this.props.page} page="donate" name="Donate"/>
                     <TocHeader activePage={this.props.page} page="cedge" name="Beta testing"/>
+                    <TocHeader activePage={this.props.page} page="merch" name="Merchandise"/>
                     <TocHeader activePage={this.props.page} page="configuration" name="Configuration"/>
                     <TocHeader activePage={this.props.page} page="permissions" name="Permissions"/>
                     {/*<TocHeader activePage={this.props.page} page="music" name="Music commands"/>*/}
