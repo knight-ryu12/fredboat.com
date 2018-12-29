0541b263431b33077b7b5939cbd9baef2eb6c192
Frederik Mikkelsen <fred@frederikam.com>
2018-12-29 15:48:17 +0100

    Render track data

--- src/playerinfo/PlayerInfo.js
+++ src/playerinfo/PlayerInfo.js
@@ -1,20 +1,53 @@
 import React, { Component } from 'react';
 import "./PlayerInfo.css";
 import PlayingBar from "./PlayingBar";
+import fallback from "./fallback.svg"
 
 class PlayerInfo extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {player: null, receivedTime: null};
+        const instance = this;
+        // noinspection JSUnresolvedVariable
+        const ws = new WebSocket("ws://localhost:1356/playerinfo/" + props.guild);
+        ws.onmessage = function (msg) {
+            instance.setState({
+                player: JSON.parse(msg.data),
+                receivedTime: new Date() / 1000
+            }, null)
+        };
+        ws.onclose = () => console.log("Websocket disconnected")
+    }
+
     render() {
+        let topTrack = null;
+        let nextTrack = null;
+        console.log(this.state.player);
+        if (this.state.player != null) {
+            topTrack = this.state.player.queue[0];
+            nextTrack = this.state.player.queue[1];
+        }
+
+        let image = null;
+        if (topTrack != null && topTrack.image != null) {
+            image = (<img className="PlayerInfo-image" src={topTrack != null ? topTrack.image : ""} alt=""/>)
+        } else {
+            image = (<img className="PlayerInfo-image" src={fallback} alt=""/>)
+        }
+
         return (
             <div className="PlayerInfo-page">
                 <div className="PlayerInfo">
-                    <img className="PlayerInfo-image" src="https://i.ytimg.com/vi/zNRbP7U0Iq8/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLDX2XAJghjO0WgVyOJAg3BIFSlMWg"/>
+                    {image}
                     <div className="PlayerInfo-right">
                         <div className="PlayerInfo-track-name">
-                            Track name
+                            {topTrack == null ? "The queue is empty" : topTrack.name}
                         </div>
-                        <PlayingBar/>
+                        <PlayingBar track={topTrack} receivedTime={this.state.receivedTime}
+                                    receivedPos={this.state.player != null ? this.state.player.playingPos : null}/>
                         <div className="PlayerInfo-coming-up">
-                            Coming up: ASD
+                            {nextTrack != null ? "Next: " + nextTrack.name : ""}
                         </div>
                     </div>
                 </div>
--- src/playerinfo/PlayingBar.js
+++ src/playerinfo/PlayingBar.js
@@ -2,15 +2,53 @@ import React, { Component } from 'react';
 
 class PlayingBar extends Component {
     render() {
+        if (this.props.track == null) {
+            return (
+                <div className="PlayingBar">
+                    00:00
+                    <div className="PlayingBar-progress">
+                        <div className="PlayingBar-inner" style={{width: "0"}}/>
+                    </div>
+                    00:00
+                </div>
+            )
+        }
+
+        const durationSec = this.props.track.duration / 1000;
+        const sinceMessage = new Date() / 1000 - this.props.receivedTime;
+        const timePlayed = this.props.receivedPos / 1000 + sinceMessage;
+        const percentComplete = (timePlayed / durationSec) * 100 + "%";
+        const useHours = durationSec >= 60*60;
+
         return (
             <div className="PlayingBar">
-                00:12
+                {PlayingBar.formatTime(timePlayed, useHours)}
                 <div className="PlayingBar-progress">
-                    <div className="PlayingBar-inner" style={{width: "33%"}}/>
+                    <div className="PlayingBar-inner" style={{width: percentComplete}}/>
                 </div>
-                01:00
+                {PlayingBar.formatTime(durationSec, useHours)}
             </div>
         )
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+         setTimeout(() => this.forceUpdate(), 100)
+    }
+
+    static formatTime(seconds, useHours) {
+        const hours = PlayingBar.format0(seconds / (60*60));
+        const minutes = PlayingBar.format0((seconds % (60*60)) / 60);
+        const remainder = PlayingBar.format0(seconds % 60);
+
+        let str = minutes + ":" + remainder;
+        if (useHours) {
+            str = hours + ":" + str
+        }
+        return str
+    }
+
+    static format0(num) {
+        return num < 10 ? "0" + Math.floor(num) : Math.floor(num)
     }
 }
 
